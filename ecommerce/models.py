f3715ebae881ee0e07e27bf456e2f54c90e80897
Paulo Ricardo de Oliveira <prdioliveira@gmail.com>
2021-07-12 20:42:37 -0300

    migrations

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -14,7 +14,7 @@ class Cliente(AbstractUser):
 
     username = None
     email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=255)
     nome = models.CharField(max_length=50)
     CPF = models.CharField(max_length=20)
     endereco = models.CharField(max_length=50)
