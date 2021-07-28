50fdb26bd1e963059f067ce94335396b7e6adcd2
Paulo Ricardo de Oliveira <prdioliveira@gmail.com>
2021-07-28 20:46:30 -0300

    tratamento de erros

--- ecommerce/utils.py
+++ ecommerce/utils.py
@@ -4,20 +4,20 @@ from rest_framework.views import exception_handler, set_rollback
 from rest_framework import status
 
 def custom_exception_handler(exc, context):
-    # # give more context on the error since DRF masks it as Not Found
-    # if isinstance(exc, Http404):
-    #     set_rollback()
-    #     return Response(str(exc))
+    # give more context on the error since DRF masks it as Not Found
+    if isinstance(exc, Http404):
+        set_rollback()
+        return Response(str(exc))
 
-    # # Call REST framework's default exception handler after specific 404 handling,
-    # # to get the standard error response.
-    # response = exception_handler(exc, context)
+    # Call REST framework's default exception handler after specific 404 handling,
+    # to get the standard error response.
+    response = exception_handler(exc, context)
 
-    # # No response means DRF couldn't handle it
-    # # Output a generic 500 in a JSON format
-    # if response is None:
-    #     set_rollback()
-    #     return Response({'detail': 'Erro de Integridade'}, status=status.HTTP_403_FORBIDDEN)
+    # No response means DRF couldn't handle it
+    # Output a generic 500 in a JSON format
+    if response is None:
+        set_rollback()
+        return Response({'detail': 'Erro de Integridade'}, status=status.HTTP_403_FORBIDDEN)
 
-    return "response"
+    return response
     
